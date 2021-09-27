642c3c19181c8e9d0cc756156ae1e88c1a92ec15
rassjo <58602536+rassjo@users.noreply.github.com>
2021-09-27 09:43:19 +0200

    Add files via upload

---
+++ Discrepancy Equation.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+#Matrix of the picture without a mask applied
+A=np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+#Matrix of the picture after it has been repaired
+B=np.arange(9).reshape(3,3)
+#Matrix of just the mask 
+M=[[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+
+
+#Number of matrix elements
+n=A.size
+#Horizontal matrix size
+h=A.shape[0]
+#Vertical matrix size
+v=A.shape[1]
+#First sum in discrepancy equation
+I1=0
+#Second sum in discrepancy equation
+I2=0
+
+#Discrepancy score program
+
+#This loop calculates the sum in the numerator of the discrepancy equation.
+for i in range(0,h):
+    for j in range(0,v):
+        #If statement ensures that only points that had the mask are considered in the equation
+        if M[i][j] == 1:
+            I1=I1+(A[i][j]-B[i][j])
+        
+#This loop calculates the sum in the denomenator of the discrepancy equation.        
+for i in range(0,h):
+    for j in range(0,v):
+        #If statement ensures that only points that had the mask are considered in the equation
+        if M[i][j] == 1:
+            Imean=(A[i][j]+B[i][j])/2
+            I2=I2+(B[i][j]-Imean)
+
+#Define the denominator in the discrepancy equation
+omega=np.sqrt(1/(n-1)*I2**2)
+
+#Calculate chi value.
+X=np.sqrt(I1**2/(n*omega**2))
+
+print(X**2)
